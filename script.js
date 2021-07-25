c3da03cc4905c2ed08e3f91d7174a50ac066eb5c
Dando513 <49767042+Dando513@users.noreply.github.com>
2021-07-24 23:05:13 -0400

    fixed bug in js code

--- script.js
+++ script.js
@@ -2,9 +2,25 @@ const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
 sounds.forEach (sound => {
     const btn = document.createElement('button')
-    btn.classList.add('btn'
+    btn.classList.add('btn')
     
     btn.innerText = sound
 
+    btn.addEventListener('click', () => {
+        stopSonds()
+
+        document.getElementById(sound).play()
+    })     
+    
     document.getElementById('buttons').appendChild(btn)
 })
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
+
